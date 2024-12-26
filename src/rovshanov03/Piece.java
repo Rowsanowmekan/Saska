b57dd0b57f783d06158074b071e93cda9e245ae2
Rowsanowmekan <bezzat2003@gmail.com>
2024-12-26 14:03:07 +0300

    Initial commit

---
+++ src/rovshanov03/Piece.java
@@ -0,0 +1,18 @@
+package rovshanov03;
+
+public enum Piece {
+    EMPTY,
+    BLACK,
+    WHITE,
+    BLACK_KING,
+    WHITE_KING;
+
+    public Piece toKing() {
+        if (this == BLACK) {
+            return BLACK_KING;
+        } else if (this == WHITE) {
+            return WHITE_KING;
+        }
+        return this; // Если уже король или EMPTY
+    }
+}
